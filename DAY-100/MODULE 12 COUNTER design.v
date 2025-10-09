b3250e43aa498fd61c47166bd217764296a0e679
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-10-09 09:40:30 +0530

    MODULE 12 COUNTER design

---
+++ DAY-100/MODULE 12 COUNTER design.v
@@ -0,0 +1,15 @@
+module mod12_counter (
+  input wire clk,
+  input wire rst_n,        // active low reset
+  output reg [3:0] count
+);
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      count <= 4'b0000;
+    else if (count == 4'd11)
+      count <= 4'b0000;
+    else
+      count <= count + 4'b0001;
+  end
+endmodule
