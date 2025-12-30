b21b8d101bc04babf8e7fda9c728f2f4082f5162
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-12-30 11:02:03 +0530

    or gate with generator tb

--- DAY - 101/or gate with generator tb.v
+++ DAY - 101/or gate with generator tb.v
@@ -12,3 +12,24 @@ module orr_tb;
     $display("y=%0b",y);
   end
 endmodule
+// module orr_tb;
+//   parameter n = 8;
+//   reg [(n-1):0] a, b;
+//   wire [(n-1):0] y;
+
+//   or_gate u1 (.a(a), .b(b), .y(y));
+//   integer i;
+//   initial begin
+//     b = 8'b1010_1010;
+//     for (i = 0; i < 3; i = i + 1) begin
+//       case (i)
+//         0: a = 8'b1111_0000;
+//         1: a = 8'b1101_0000;
+//         2: a = 8'b1001_0000;
+//       endcase
+//       #10;
+//       $display("a=%b, b=%b, y=%b", a, b, y);
+//     end
+//     $finish;
+//   end
+// endmodule
