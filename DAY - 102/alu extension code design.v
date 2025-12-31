4e71d361339a7be904fcb69397bbc967cf65410d
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-12-31 11:44:43 +0530

    alu extension code design

---
+++ DAY - 102/alu extension code design.v
@@ -0,0 +1,17 @@
+`define ADD  4'b0000
+`define SUB  4'b0001
+`define MUL  4'b0010
+`define DIV  4'b0011
+`define INC  4'b0100
+`define DEC  4'b0101
+`define AND  4'b0110
+`define OR   4'b0111
+`define NOT  4'b1000
+`define BUF  4'b1001
+`define XOR  4'b1010
+`define XNOR 4'b1011
+`define NAND 4'b1100
+`define NOR  4'b1101
+`define MOD  4'b1110
+`define RSH  4'b1111
+//here we are created the new file in the design design side with the .v extension
